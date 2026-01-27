6271eae238157c1c478a3e251dacf1c4a8440d37
Brielle Harrison <nyteshade@gmail.com>
2026-01-26 19:21:07 -0800

    Fixing scripts to run on other operating systems, not just macOS

--- install.sh
+++ install.sh
@@ -1,18 +1,22 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 printf "\x1b[1mBuilding scriptsort and ms\x1b[22m..."
 ./build.sh
 
-[[ ${RC} -eq 0 ]] && echo "done" || echo "\x1b[31mfailed\x1b[39m"
+if [[ ${RC} -eq 0 ]]; then
+  echo "done"
+else
+  printf "\x1b[31mfailed\x1b[39m"
+fi
 
 
 printf "\x1b[1mInstalling files\x1b[22m...\n\n"
 
 if [[ ! -d ${HOME}/.local ]]; then
-  mkdir -p ${HOME}/.local
+  mkdir -p "${HOME}/.local"
 fi
 
-cp -rpv .local/* ${HOME}/.local
+cp -rpv .local/* "${HOME}/.local"
 
 if [[ ${SHELL} =~ "zsh" ]]; then
   printf "\x1b[1mInstalling ZSH support...\x1b[22m"
